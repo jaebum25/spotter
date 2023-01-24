b94fa557c7f043ce27c969349b23ac88d1faf7e3
Jaebum Lee <jaebumlee@Jaebums-MacBook-Pro.local>
2023-01-24 13:36:52 -0500

    fix tbody FeedPost

--- src/components/FeedPost/FeedPost.js
+++ src/components/FeedPost/FeedPost.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function FeedPost({ post, handleDelete }) {
   return (
-    <table>
+    <tbody>
       <tr>
         <td>{post.name}</td>
         <td><button onClick={() => handleDelete(post.id)}>X</button></td>
       </tr>
-    </table>
+    </tbody>
   )
 }
